f08c9dfd45c1737a80dc3c050e2fa4c8adde6b3e
Juan-Betancourt <juan.c.betanciurtgit config --global user.email juan.c.betancourt@hotmail.com>
2018-10-03 20:33:11 -0700

    ran first test -- PASSED

--- 01-node-ecosystem/lab/__test__/greet.spec.js
+++ 01-node-ecosystem/lab/__test__/greet.spec.js
@@ -1,3 +1,20 @@
 'use strict';
 
-const hello = require('./lib/greet.js');
+const hello = require('../lib/greet.js');
+
+const faker = require('faker');
+
+describe('Greet Module', () => {
+  it('Write a test that should return null if the value is not a string', () => {
+    let greetPerson = hello.greet(1);
+    expect(greetPerson).toBe(null);
+  });
+
+
+
+
+
+
+
+
+});
